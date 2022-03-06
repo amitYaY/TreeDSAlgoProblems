a0276a633f789da14d1687f6d5ee1e399a4616fd
Amit Upadhyay <upadhyay_amit@yahoo.com>
2022-03-06 17:39:00 +0530

    Correction

--- src/main/java/com/ds/algo/one/SolutionService2.java
+++ src/main/java/com/ds/algo/one/SolutionService2.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class SolutionService {
+public class SolutionService2 {
 
 	public static void main(String[] args) {
 
